03310e12559075baaa6969484f129f7f1b4ff08b
Sambhram <sambhramnaregal22@gmail.com>
2025-11-23 20:24:24 +0530

    url

--- backend/src/api.py
+++ backend/src/api.py
@@ -19,12 +19,13 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://smartdocfinder-ai-powered-document.onrender.com",
+        "https://smartdocfinder-ai-powered-document-d7lg.onrender.com",
     ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
 
 
 class SearchRequest(BaseModel):
